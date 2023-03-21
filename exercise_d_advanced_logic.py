c6afccf3290dd8fdaba46f3bba1a67910fa0d855
Cooper-Higgins <jamie@supplantree.com>
2023-03-21 18:59:07 +0000

    Attempted up to #3 in Exercise D

--- exercise_d_advanced_logic.py
+++ exercise_d_advanced_logic.py
@@ -3,13 +3,21 @@
 numbers = [1, 6, 2, 2, 7, 1, 6, 13, 99, 7]
 
 # 1. Print out a list of the even integers:
+even_numbers = []
 
+for number in numbers:
+    if(number % 2 == 0):
+      even_numbers.append(number)
+print(even_numbers)
 
 # 2. Print the difference between the largest and smallest value:
-
+numbers.sort()
+smallest_value = numbers[0]
+largest_value = numbers[-1]
+difference = largest_value - smallest_value
+print(difference)
 
 # 3. Print True if the list contains a 2 next to a 2 somewhere.
-
 
 # 4. Print the sum of the numbers, 
 #    BUT ignore any section of numbers starting with a 6 and extending to the next 7.
