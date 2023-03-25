cfa18534f791ce5329e73db8697efa053a704cff
Cooper-Higgins <jamie@supplantree.com>
2023-03-25 10:58:36 +0000

    Starting from scratch again, deleted solutions and starting fresh to revise

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -55,41 +55,21 @@ users = {
 }
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
-print(users["Joanthan"]["twitter"])
 
 # 2. Get Erik's hometown
-print(users["Erik"]["home_town"])
 
 # 3. Get the list of Erik's lottery numbers
-print(users["Erik"["lottery_numbers"]])
 
 # 4. Get the species of Avril's pet Monty
-avril_pet_species = users["Avril"]["pets"][0]["species"]
-print (avril_pet_species)
 
 # 5. Get the smallest of Erik's lottery numbers
-min(users["Erik"]["lottery_numbers"])
 
 # 6. Return a list of Avril's lottery numbers that are even
-avril_lottery = users["Avril"]["lottery_numbers"]
-even_avril_lottery = []
-
-for number in avril_lottery: 
-    if(number % 2 == 0):
-      even_avril_lottery.append(number)
-print(even_avril_lottery)
 
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
-users["Erik"]["lottery_numbers"].append(7)
 
 # 8. Change Erik's hometown to Edinburgh
-users["Erik"]["home_town"] = "Edinburgh"
 
 # 9. Add a pet dog to Erik called "fluffy"
-users["Erik"]["pets"].append({"species: dog, name: fluffy"})
 
 # 10. Add another person to the users dictionary
-jane = {"name": "jane", 
-        "twitter": "janedoe", 
-        "home_town": "wick"}
-print(jane)
--- exercise_d_advanced_logic.py
+++ exercise_d_advanced_logic.py
@@ -3,19 +3,9 @@
 numbers = [1, 6, 2, 2, 7, 1, 6, 13, 99, 7]
 
 # 1. Print out a list of the even integers:
-even_numbers = []
 
-for number in numbers:
-    if(number % 2 == 0):
-      even_numbers.append(number)
-print(even_numbers)
 
 # 2. Print the difference between the largest and smallest value:
-numbers.sort()
-smallest_value = numbers[0]
-largest_value = numbers[-1]
-difference = largest_value - smallest_value
-print(difference)
 
 # 3. Print True if the list contains a 2 next to a 2 somewhere.
 
@@ -24,6 +14,18 @@ print(difference)
 #    
 #    So [11, 6, 4, 99, 7, 11] would have sum of 22
 
+sum = 0
+is_true = False
+for number in numbers:
+  if is_true == True:
+    if number == 7:
+       is_true = False
+  elif number == 6:
+      is true = True
+  else:
+   sum += number
+
+print(sum)
 
 # 5. HARD! Print the sum of the numbers. 
 #    Except the number 13 is very unlucky, so it does not count.
@@ -32,6 +34,15 @@ print(difference)
 #
 #    So [5, 13, 2] would have sum of 5. 
 
+index = 0
+total = 0
+for number in numbers:
+   if number == 13 or numbers[index - 1] == 13:
+      pass
+   else:
+      total += number
+    index += 1
+  print(total)
 
 
 
